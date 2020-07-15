688c07c8ed6d8e29b219c1e746d302e279e099fb
Son Nguyen <trungson077@gmail.com>
2020-07-15 10:54:30 +0700

    Update list_name.py

--- list_name.py
+++ list_name.py
@@ -40,15 +40,15 @@ def append_names(frame):
         return face_names
 
         #Draw rectangle in faces 
-        # for (top, right, bottom, left), name in zip(face_locations, face_names):
-            # top *= 4
-            # right *= 4
-            # bottom *= 4
-            # left *= 4
-            # cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            # cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            # font = cv2.FONT_HERSHEY_DUPLEX
-            # cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+        for (top, right, bottom, left), name in zip(face_locations, face_names):
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+            font = cv2.FONT_HERSHEY_DUPLEX
+            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
 while True:  
     ret, frame = video_capture.read()
